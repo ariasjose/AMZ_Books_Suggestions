346c5d5ae253ff9116c853bfbd4bbe7a0a4b218c
Jose Arias <jose.ariasq@gmail.com>
2018-04-05 14:17:03 -0600

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "AMZ_VG_Suggestions"
+version := "0.1"
+scalaVersion := "2.10.7"
+
+libraryDependencies += "org.apache.spark" % "spark-core_2.10" % "1.6.2"
+libraryDependencies += "org.apache.spark" % "spark-sql_2.10" % "1.6.2"
+libraryDependencies += "org.apache.spark" % "spark-mllib_2.10" % "1.6.2"
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,98 @@
+  /**
+    * Created by jarias on 4/4/2018.
+    */
+
+  import org.apache.hadoop.fs.{FileSystem, Path}
+  import org.apache.spark.ml.feature.{StringIndexer}
+  import org.apache.spark.mllib.recommendation.{ALS, MatrixFactorizationModel, Rating}
+  import org.apache.spark.ml.{Pipeline}
+  import org.apache.spark.sql.SQLContext
+  import org.apache.spark.SparkConf
+  import org.apache.spark.SparkContext
+
+  object Main {
+
+    //case class Product(Name:String)
+
+    def main(args: Array[String]): Unit = {
+
+      val conf = new SparkConf().setAppName("AMZVGSE")
+                  //.setMaster("yarn-client")
+
+      val sc = new SparkContext(conf)
+      val sqlContext = new SQLContext(sc)
+
+
+      //Import video games ratings (userId, productId, rating) - user and product ids are alphanumeric
+      val videoGamesRDD = sc.textFile("file:///C:/Spark/data/AMZ/ratingsVideoGames.csv").map( r => {
+          val values = r.split(',')
+          (values(0), values(1), values(2))
+      })
+
+      //Convert to DF
+      import sqlContext.implicits._
+      val videoGamesDF = videoGamesRDD.toDF(colNames = "userId", "productId", "rating")
+
+      //Create string indexers to map alphanumeric to Int as Rating class expects Int
+      //types for user and product ids
+      val strIndexerForUser = new StringIndexer().setInputCol("userId").setOutputCol("userIdInt")
+      val strIndexerForProduct = new StringIndexer().setInputCol("productId").setOutputCol("productIdInt")
+
+      val pipeline = new Pipeline().setStages(Array(strIndexerForUser, strIndexerForProduct))
+      val pipelineModel = pipeline.fit(videoGamesDF)
+      val videoGamesDFWithIdsMapped = pipelineModel.transform(videoGamesDF)
+
+      //|userId|productId|rating|userIdInt|productIdInt
+      //|||||
+      //|||||
+
+      //Create rdd of Rating from DF and persist it as we'll use it later
+      val videoGamesRatingsRdd = videoGamesDFWithIdsMapped.map( r => {
+          Rating(r.getAs[Double]("userIdInt").toInt,
+                  r.getAs[Double]("productIdInt").toInt,
+                  r.getAs[String]("rating").toDouble)
+      }).cache
+
+      //Display data description
+      var totalOfReviews = videoGamesRatingsRdd.count
+      var totalReviewers = videoGamesRatingsRdd.map(_.user).distinct.count
+      var totalGames = videoGamesRatingsRdd.map(_.product).distinct.count
+      println(s"Total Reviews: $totalOfReviews; Total Reviewers: $totalReviewers; Games rated: $totalGames")
+
+      //Split ratings RDD: training data = 80% ;  test data = 20%
+      val ratingsSplit = videoGamesRatingsRdd.randomSplit(weights=Array(0.8, 0.2), seed=0L)
+      val trainingDataRdd = ratingsSplit(0).cache
+      val testDataRdd = ratingsSplit(1).cache
+
+      //Display training and test data
+      val trainingSize = trainingDataRdd.count
+      val testSize = testDataRdd.count
+      println(s"Training size: $trainingSize; Test size: $testSize")
+
+
+      //Train and create model
+      val rank = 5
+      val iterations = 20
+      val matrixModel = ALS.train(trainingDataRdd, rank, iterations)
+
+      //Make predictions using the model for the test data
+      val predictionsForTestData = matrixModel.predict(
+            testDataRdd.map(r => (r.user, r.product))
+      )
+
+      //Validate our model by comparing predicted and actual ratings for test data user
+      //Will use join and then validate our model using Mean Absolute Error
+      val predictedRatingsRdd = predictionsForTestData.map( r => ((r.user, r.product), r.rating))
+      val testDataRatingsRdd = testDataRdd.map( r => ((r.user, r.product), r.rating))
+      val predictedAndActualRatingRdd = predictedRatingsRdd.join(testDataRatingsRdd)
+
+      //Get the MAE amd display it
+      val mae = predictedAndActualRatingRdd.map({
+          case ((user, product), (ratingP, ratingT)) => math.abs(ratingP - ratingT)
+      }).mean
+
+      println(s"MAE for our model: $mae")
+
+
+    }
+  }
